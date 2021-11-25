6ceefcb4d7965bcb69a34c0e7407da48e0cc44e7
Diogo Martins <diogomartins200214@gmail.com>
2021-11-25 16:12:15 +0000

    Implemented mailing using nodemailer

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,44 @@
 import express from "express";
+import nodemailer from 'nodemailer'
 
 import authRouter from "./authRoutes";
 import adminRouter from "./adminRoutes";
 import publicRouter from "./publicRoutes";
+import { configMail } from "src/utils/emailData";
 
 export const router = express();
 
 router.use("/auth", authRouter);
 
 router.use("/admin", adminRouter);
+
+// Teste 
+
+router.post("/reset", function (req, res, next) {
+  const data = req.body;
+  console.log(data);
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: configMail.mailUser,
+      pass: configMail.mailPass,
+    },
+  });
+
+  var mailOptions = {
+    from: configMail.mailFrom,
+    to: data.to,
+    subject: data.subject,
+    text: data.texts
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email enviado: " + info.response);
+      res.send("<b>Email enviado</b> <br>" + info.response);
+    }
+  });
+});
---
+++ src/utils/emailData.ts
@@ -0,0 +1,5 @@
+export const configMail = {
+  mailUser: "goldenskinhotel@gmail.com",
+  mailPass: "pelourinhostudio",
+  mailFrom: '"Golden Skin Hotel" goldenskinhotel@gmail.com'
+}
