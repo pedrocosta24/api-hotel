ff5f5b04bfe07bd8a5c3405266b57002a2823632
Pedro Costa <pedrocosta2406@protonmail.com>
2021-12-18 22:22:56 +0000

    Added route to filter rooms via query params

--- src/controllers/RoomsController.ts
+++ src/controllers/RoomsController.ts
@@ -35,7 +35,8 @@ export default class RoomsController {
       })
         .sort([[req.query.orderBy, req.query.direction]])
         .limit(ITEMS_PER_PAGE)
-        .skip((page - 1) * ITEMS_PER_PAGE);
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .getFilter();
     } catch (err) {
       console.error(err);
     }
@@ -45,7 +46,7 @@ export default class RoomsController {
     try {
       const { id } = req.params;
 
-      Room.find({ _id: id }, (err: Error, room: IRoom) => {
+      Room.findById(id, (err: Error, room: IRoom) => {
         if (room) {
           res.status(200).json(room);
         } else {
@@ -68,6 +69,150 @@ export default class RoomsController {
           res.status(400);
         }
       });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async findAvailableRooms(req: Request, res: Response) {
+    try {
+      const { page = 1 }: any = req.query;
+      const { checkIn, checkOut }: any = req.query;
+
+      Room.find(
+        {
+          $or: [
+            {
+              $nor: [
+                {
+                  "reserved.to": {
+                    $gte: new Date(checkIn),
+                  },
+                  "reserved.from": {
+                    $lte: new Date(checkOut),
+                  },
+                },
+              ],
+            },
+            {
+              reserved: { $eq: [] },
+            },
+          ],
+        },
+        (err: Error, rooms: IRoom) => {
+          if (rooms) {
+            res.status(200).json(rooms);
+          } else {
+            res.status(400);
+          }
+        }
+      )
+        .sort([[req.query.orderBy, req.query.direction]])
+        .limit(ITEMS_PER_PAGE)
+        .skip((page - 1) * ITEMS_PER_PAGE);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async filterRooms(req: Request, res: Response) {
+    try {
+      const { page = 1 }: any = req.query;
+      const { checkIn, checkOut }: any = req.query;
+      const q = req.query;
+
+      let filter = {};
+
+      if (q.checkIn && q.checkOut) {
+        filter = {
+          ...filter,
+          $or: [
+            {
+              $nor: [
+                {
+                  "reserved.to": {
+                    $gte: new Date(checkIn),
+                  },
+                  "reserved.from": {
+                    $lte: new Date(checkOut),
+                  },
+                },
+              ],
+            },
+            {
+              reserved: { $eq: [] },
+            },
+          ],
+        };
+      }
+
+      if (q.type) {
+        let types: any = q.type;
+
+        filter = {
+          ...filter,
+          type: {
+            $in: types.split(","),
+          },
+        };
+      }
+
+      if (q.capacityLT || q.capacityGT) {
+        filter = {
+          ...filter,
+          capacity: {
+            $gte: q.capacityGT ? q.capacityGT : 0,
+            $lte: q.capacityLT ? q.capacityLT : 100,
+          },
+        };
+      }
+
+      if (q.no_bedsLT || q.no_bedsGT) {
+        filter = {
+          ...filter,
+          no_beds: {
+            $gte: q.no_bedsGT ? q.no_bedsGT : 1,
+            $lte: q.no_bedsLT ? q.no_bedsLT : 100,
+          },
+        };
+      }
+
+      if (q.priceLT || q.priceGT) {
+        filter = {
+          ...filter,
+          price_night: {
+            $gte: q.priceGT ? q.priceGT : 0,
+            $lte: q.priceLT ? q.priceLT : 999999,
+          },
+        };
+      }
+
+      if (q.amenities) {
+        let amenities: any = q.amenities;
+
+        filter = {
+          ...filter,
+          $expr: {
+            $and: amenities.split(",").map((amenity: any) => {
+              return {
+                $eq: [`$amenities.${amenity}`, true],
+              };
+            }),
+          },
+        };
+      }
+
+      Room.find(filter, (err: Error, rooms: IRoom) => {
+        if (rooms) {
+          res.status(200).json(rooms);
+        } else {
+          res.status(400);
+        }
+      })
+        .sort([[q.orderBy, q.direction]])
+        .limit(ITEMS_PER_PAGE)
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .getFilter();
     } catch (err) {
       console.error(err);
     }
--- src/routes/publicRoutes.ts
+++ src/routes/publicRoutes.ts
@@ -4,7 +4,9 @@ import RoomsController from "@controllers/RoomsController";
 const router = express.Router();
 const roomsController = new RoomsController();
 
-router.post("/rooms", roomsController.findAll); // Get all rooms
-router.get("/rooms/number/:number", roomsController.findByRoomNo); // Get room by room_no
+router.get("/rooms", roomsController.findAll); // Get all rooms
+router.get("/rooms/filter", roomsController.filterRooms); // Filter rooms via query params
+router.get("/rooms/id/:id", roomsController.findByID); // Get room by room_no
+router.get("/rooms/available", roomsController.findAvailableRooms); // Get available rooms outside a date range
 
 export default router;
