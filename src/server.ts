8babd41fc227ca4608fc38dbd948a0b495a012d7
Diogo Martins <diogomartins200214@gmail.com>
2021-12-26 00:17:29 +0000

    Cors everywhere

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { router } from "./routes/router";
 import "./database/connection";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: '*'
+}))
 
 app.use(router);
 
