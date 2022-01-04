68e78cbfe8beef87aebd5616f82f81ce82b7bdbf
Pedro Costa <pedrocosta2406@protonmail.com>
2022-01-04 13:25:58 +0000

    Updated SendEmailController with vercel link

--- src/controllers/SendEmailController.ts
+++ src/controllers/SendEmailController.ts
@@ -25,6 +25,7 @@ export default class SendEmailController {
         }).save();
       }
 
+      //https://golden-skin-hotel.vercel.app
       const link = `https://golden-skin-hotel.vercel.app/auth/password-reset/${user._id}/${token.token}`;
       await sendEmail(
         user.email,
@@ -34,7 +35,7 @@ export default class SendEmailController {
 
       res.send("password reset link sent to your email account");
     } catch (error) {
-      res.send("An error occurred");
+      res.status(400).send("An error occurred");
       console.log(error);
     }
   }
@@ -45,22 +46,21 @@ export default class SendEmailController {
       const { error } = schema.validate(req.body);
       if (error) return res.status(400).send(error.details[0].message);
 
-      const user = await UserModel.findById(req.params.userId);
-      if (!user) return res.status(400).send("invalid link or expired");
-
-      const token = await TokenModel.findOne({
-        userId: user._id,
-        token: req.params.token,
+      await UserModel.findById(req.params.userId).then((user: any) => {
+        if (!user) return res.status(400).send("user not found");
+        TokenModel.findOne({
+          userId: user._id,
+          token: req.params.token,
+        }).then(async (token: any) => {
+          if (!token) return res.status(400).send("Invalid link or expired");
+          user.password = await bcrypt.hash(req.body.password, 10);
+          await user.save();
+          await token.delete();
+          res.status(200).send("password reset successfully.");
+        });
       });
-      if (!token) return res.status(400).send("Invalid link or expired");
-
-      user.password = await bcrypt.hash(req.body.password, 10);
-      await user.save();
-      await token.delete();
-
-      res.send("password reset successfully.");
     } catch (error) {
-      res.send("An error occurred");
+      res.status(400).send("An error occurred");
       console.log(error);
     }
   }
