688a74f980609508e15714e1ba6135d541daa70a
Pedro Costa <pedrocosta2406@protonmail.com>
2022-01-06 17:03:10 +0000

    Nodemailer auth

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -9,9 +9,7 @@ export const sendEmail = async (
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      port: 465,
       secure: true,
-
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
